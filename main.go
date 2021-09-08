26e7089b168e62cec64077735c350a030709465a
Kevin McDonald <kevin@simulmedia.com>
2021-09-08 10:13:13 -0400

    a bit more robust

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 	"net"
 	"os"
 	"regexp"
+	"sort"
+	"strings"
+	"sync"
+	"time"
 )
 
 func main() {
@@ -19,19 +23,20 @@ func main() {
 	defer l.Close()
 	fmt.Println("Listening on", addr)
 
-	server := ChatServer{Listener: l}
+	server := ChatServer{listener: l}
 	server.Start()
 }
 
 type ChatServer struct {
-	Listener net.Listener
+	listener net.Listener
 	users    map[string]net.Conn
+	mutex    sync.Mutex
 }
 
 func (s *ChatServer) Start() {
 	s.users = make(map[string]net.Conn)
 	for {
-		c, err := s.Listener.Accept()
+		c, err := s.listener.Accept()
 		if err != nil {
 			fmt.Println(err)
 			continue
@@ -42,8 +47,13 @@ func (s *ChatServer) Start() {
 			fmt.Println("Unable to make handshake:", err)
 			continue
 		}
+
+		s.broadcast(fmt.Sprintf("User @%s has entered the chat", u))
+		s.mutex.Lock()
 		s.users[u] = c
+		s.handleMessage(u, "/users")
 		go s.handleUserConnection(u, c)
+		s.mutex.Unlock()
 	}
 }
 
@@ -54,13 +64,13 @@ func handshake(conn net.Conn) (string, error) {
 		return "", err
 	}
 
-	rx := regexp.MustCompile("^<Connect:@([A-Za-z0-9_-]+)>")
+	rx := regexp.MustCompile(`^<Connect:@([A-Za-z0-9_-]+)>`)
 	if !rx.Match(buf) {
 		ack(conn, "ERR")
 		return "", errors.New(fmt.Sprintf("Invalid connection header <%s>", buf))
 	}
 	uid := rx.ReplaceAllString(string(buf[:l]), "$1")
-	fmt.Printf("User <%s> connected!\n", uid)
+	fmt.Printf("User %s connected!\n", uid)
 	ack(conn, "OK")
 
 	return uid, nil
@@ -73,7 +83,25 @@ func ack(conn net.Conn, msg string) error {
 	return nil
 }
 
+func (s *ChatServer) online() []string {
+	var users []string
+	for u, l := range s.users {
+		if l != nil {
+			users = append(users, u)
+		}
+	}
+	sort.Strings(users)
+	return users
+}
+
 func (s *ChatServer) handleUserConnection(user string, conn net.Conn) {
+	defer func() {
+		s.mutex.Lock()
+		delete(s.users, user)
+		s.mutex.Unlock()
+		s.broadcast(fmt.Sprintf("User @%s has left the chat", user))
+	}()
+
 	buf := make([]byte, 1024)
 	for {
 		l, err := conn.Read(buf)
@@ -82,12 +110,41 @@ func (s *ChatServer) handleUserConnection(user string, conn net.Conn) {
 			return
 		}
 
-		msg := fmt.Sprintf("@%s: %s", user, string(buf[:l]))
-		fmt.Println(msg)
+		s.handleMessage(user, string(buf[:l]))
+	}
+}
+
+func (s *ChatServer) handleMessage(sender, message string) {
+	ts := ts()
+	if message == "/users" {
+		m := fmt.Sprintf("[%s] Online: %s", ts, strings.Join(s.online(), ", "))
+		s.users[sender].Write([]byte(m))
+	} else if message[0] == '@' {
+		rx := regexp.MustCompile(`^@([^\s]+)\s+(.*)`)
+		if match := rx.FindStringSubmatch(message); len(match) == 3 {
+			if c, ok := s.users[match[1]]; ok {
+				c.Write([]byte(fmt.Sprintf("[%s] @%s <private>: %s", ts, sender, match[2])))
+			} else {
+				s.users[sender].Write([]byte(fmt.Sprintf("[%s] User not online", ts)))
+			}
+		}
+	} else {
+		m := fmt.Sprintf("[%s] @%s: %s", ts, sender, message)
+		fmt.Println(m)
 		for u, c := range s.users {
-			if u != user {
-				c.Write([]byte(msg))
+			if u != sender {
+				c.Write([]byte(m))
 			}
 		}
 	}
+}
+
+func (s *ChatServer) broadcast(message string) {
+	for _, c := range s.users {
+		c.Write([]byte(fmt.Sprintf("[%s] system: %s", ts(), message)))
+	}
+}
+
+func ts() string {
+	return time.Now().Format("15:04:05")
 }
